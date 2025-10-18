db5e9dc07c1369e12f74a333212f6cad6aab630a
Aman Ansari <amanansari@Amans-Mac-mini.local>
2025-10-18 21:22:37 +0530

    added static dir

--- mpbilecrm3/settings.py
+++ mpbilecrm3/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -135,6 +135,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = '../static_root'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, '../static_root')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
